eb72b67c5e7001fc83dc787e6a2c5dccee020850
Joao Bione <joaph@ciandt.com>
2023-02-01 13:51:30 -0300

    chore: images dir and some structure

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './Header.css';
+
+const Header = (props) => {
+    return (
+        <div className={"Wrapper"}>
+            <span className={"Name"}>Name: {props.name} {props.surname}</span>
+            <span className={"Classes"}>Class: {props.classes}</span>
+        </div>
+    )
+}
+
+export default Header;
